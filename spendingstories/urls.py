e3d7b7f23278c71c363346eaa192b7808c0c8e6b
Bellon Pierre <bellon.pierre@gmail.com>
2013-07-23 10:52:07 +0200

    Commented core url into main url file (spendingstories/urls.py) + improved other urls

--- spendingstories/urls.py
+++ spendingstories/urls.py
@@ -1,16 +1,15 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import redirect_to
 
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',    
     # Our main views URL, see spendingstories.core.urls 
-    url(r'^$', redirect_to, {'url' : '/spending/'})
-    url(r'^spending/', include('spendingstories.core.urls')),
+    # url(r'^spending/',  include('spendingstories.core.urls')),
     # API & API documentation urls 
-    url(r'^api/', include('spendingstories.api.urls')),
-    url(r'^api-docs/', include('rest_framework_docs.url')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/',       include('spendingstories.api.urls')),
+    url(r'^api-docs/',  include('rest_framework_docs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/',     include(admin.site.urls)),
 )
