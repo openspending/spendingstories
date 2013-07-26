dd8e59abf434256617430693c2c114f0bbff1769
Bellon Pierre <bellon.pierre@gmail.com>
2013-07-26 17:36:00 +0200

    Changed timezone & added django_filter to install app in settings.py

--- spendingstories/settings.py
+++ spendingstories/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -62,7 +62,7 @@ MEDIA_URL = ''
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
+# Example: ""
 STATIC_ROOT = ''
 
 # URL prefix for static files.
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
+    'django_filters',
     'rest_framework',
     'rest_framework_docs',
     'spendingstories.api',
