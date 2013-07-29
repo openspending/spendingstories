a1bff9bd8e67e0c3d9385157e1d94444ae416811
Bellon Pierre <bellon.pierre@gmail.com>
2013-07-29 15:03:40 +0200

    Added api-route url for API description

--- spendingstories/api/urls.py
+++ spendingstories/api/urls.py
@@ -5,6 +5,7 @@ import views
 
 
 urlpatterns = patterns('',
+    url(r'^$', views.api_root_view, name='api-root'),
     url(r'^stories/$', views.StoryListAPIView.as_view(), name='stories-list'),
     url(r'^stories/proximity/?', views.ProximityStoryListAPIView.as_view(), name='proximity-stories-list'), 
     # url(r'^stories/(?P<pk>[0-9]+)/compare$', views.SpendingDetailsComparing.as_view()),
