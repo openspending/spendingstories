5ab30629edeb9519252ba8cd711d12b4df28b65d
Bellon Pierre <bellon.pierre@gmail.com>
2013-07-29 16:02:19 +0200

    Added story details url into api/urls.py

--- spendingstories/api/urls.py
+++ spendingstories/api/urls.py
@@ -6,7 +6,7 @@ import views
 
 urlpatterns = patterns('',
     url(r'^$', views.api_root_view, name='api-root'),
-    url(r'^stories/$', views.StoryListAPIView.as_view(), name='stories-list'),
+    url(r'^stories/?$', views.StoryListAPIView.as_view(), name='stories-list'),
     url(r'^stories/proximity/?', views.ProximityStoryListAPIView.as_view(), name='proximity-stories-list'), 
-    # url(r'^stories/(?P<pk>[0-9]+)/compare$', views.SpendingDetailsComparing.as_view()),
+    url(r'^stories/(?P<pk>[0-9]+)/?$', views.StoryDetailsAPIView.as_view(), name='story-details'),
 )
