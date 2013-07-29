ae12453520f6e0b81915d72d97eb7a220e99245f
Bellon Pierre <bellon.pierre@gmail.com>
2013-07-29 16:58:39 +0200

    Added include to core.urls into spendingstories.urls

--- spendingstories/urls.py
+++ spendingstories/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',    
     # Our main views URL, see spendingstories.core.urls 
-    # url(r'^spending/',  include('spendingstories.core.urls')),
+    url(r'^',           include('spendingstories.core.urls')),
     # API & API documentation urls 
     url(r'^api/',       include('spendingstories.api.urls')),
     url(r'^api-docs/',  include('rest_framework_docs.urls')),
