49922772093a86d0b8b3acebfe7b7cd61e862a8a
Edouard <edou4rd@gmail.com>
2013-08-05 18:11:48 +0200

    currency

---
+++ spendingstories/currency/admin.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# Encoding: utf-8
+# -----------------------------------------------------------------------------
+# Project : OKF - Spending Stories
+# -----------------------------------------------------------------------------
+# Author : Edouard Richard                                  <edou4rd@gmail.com>
+# -----------------------------------------------------------------------------
+# License : proprietary journalism++
+# -----------------------------------------------------------------------------
+# Creation : 05-Aug-2013
+# Last mod : 05-Aug-2013
+# -----------------------------------------------------------------------------
+from django.contrib import admin
+import models 
+
+admin.site.register(models.Currency)
---
+++ spendingstories/currency/models.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Encoding: utf-8
+# -----------------------------------------------------------------------------
+# Project : OKF - Spending Stories
+# -----------------------------------------------------------------------------
+# Author : Edouard Richard                                  <edou4rd@gmail.com>
+# -----------------------------------------------------------------------------
+# License : proprietary journalism++
+# -----------------------------------------------------------------------------
+# Creation : 05-Aug-2013
+# Last mod : 05-Aug-2013
+# -----------------------------------------------------------------------------
+from django.db import models
+
+class Currency(models.Model):
+    iso_code = models.CharField(primary_key=True, max_length=3)
+    name     = models.CharField(max_length=120)
+    rate     = models.FloatField()
+
+    def __unicode__(self):
+        return "%s - %s" % (self.name, self.iso_code)
