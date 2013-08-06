43c66468bef968772eb499dd1a68044fc5faf658
Edouard <edou4rd@gmail.com>
2013-08-06 19:23:25 +0200

    Story form: check if year is available

--- webapp/core/forms.py
+++ webapp/core/forms.py
@@ -13,6 +13,11 @@
 from django import forms
 from webapp.core.models import Story, Theme
 from django.utils.translation import ugettext_lazy as _
+import json
+import os
+from django.conf import settings
+
+AVAILABLE_YEAR_PER_COUNTRY = json.load(file(os.path.join(settings.ROOT_PATH, 'data/years_available_per_country.json')))
 
 class StoryForm(forms.ModelForm):
 	class Meta:
@@ -21,5 +26,14 @@ class StoryForm(forms.ModelForm):
 	def clean(self):
 		cleaned_data = super(StoryForm, self).clean()
 		return cleaned_data
+
+	def clean_year(self):
+		year  = self.cleaned_data['year']
+		years = AVAILABLE_YEAR_PER_COUNTRY[self.cleaned_data['country']]
+		print year, years
+		if not year in years:
+			raise forms.ValidationError("For this country, are available: %s" % ", ".join(years))
+		return year
+
 
 # EOF
