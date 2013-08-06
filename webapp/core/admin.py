d6850efb8828856a7c1504c4051c67bdc565e3f9
Edouard <edou4rd@gmail.com>
2013-08-06 19:23:04 +0200

    admin

--- webapp/core/admin.py
+++ webapp/core/admin.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+# Encoding: utf-8
+# -----------------------------------------------------------------------------
+# Project : OKF - Spending Stories
+# -----------------------------------------------------------------------------
+# Author : Edouard Richard                                  <edou4rd@gmail.com>
+# -----------------------------------------------------------------------------
+# License : proprietary journalism++
+# -----------------------------------------------------------------------------
+# Creation : 06-Aug-2013
+# Last mod : 06-Aug-2013
+# -----------------------------------------------------------------------------
 from django.contrib import admin
 import models
 import forms
@@ -7,10 +19,9 @@ class ThemeAdmin(admin.ModelAdmin):
 
 class StoryAdmin(admin.ModelAdmin):
     list_display    = ('title', 'value', 'currency', 'current_value_usd', 'continuous', 'country', 'sticky', 'published')
-    readonly_fields = ('current_value_usd', 'inflation_last_year',)
+    readonly_fields = ('current_value', 'current_value_usd', 'inflation_last_year',)
     search_fields   = ['title', 'value', 'current_value_usd', 'country']
     form            = forms.StoryForm
-
 
 admin.site.register(models.Story, StoryAdmin)
 admin.site.register(models.Theme, ThemeAdmin)
