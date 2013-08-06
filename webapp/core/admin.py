8d9a99508cd99bc8b15b385423aa499a86ce3337
Edouard <edou4rd@gmail.com>
2013-08-06 17:55:26 +0200

    admin

--- webapp/core/admin.py
+++ webapp/core/admin.py
@@ -5,7 +5,10 @@ class ThemeAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 class StoryAdmin(admin.ModelAdmin):
-	readonly_fields = ('current_value_usd', 'inflation_last_year')
+	list_display    = ('title', 'value', 'currency', 'current_value_usd', 'continuous', 'country', 'sticky', 'published')
+	readonly_fields = ('current_value_usd', 'inflation_last_year',)
+	search_fields   = ['title', 'value', 'current_value_usd', 'country']
+
 
 admin.site.register(models.Story, StoryAdmin)
 admin.site.register(models.Theme, ThemeAdmin)
