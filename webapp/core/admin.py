6a0a3f483e1725b980cba549e1159e155687a8df
Edouard <edou4rd@gmail.com>
2013-08-06 18:33:01 +0200

    indent

--- webapp/core/admin.py
+++ webapp/core/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
-import models 
+import models
+import forms
 
 class ThemeAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
 
 class StoryAdmin(admin.ModelAdmin):
-	list_display    = ('title', 'value', 'currency', 'current_value_usd', 'continuous', 'country', 'sticky', 'published')
-	readonly_fields = ('current_value_usd', 'inflation_last_year',)
-	search_fields   = ['title', 'value', 'current_value_usd', 'country']
+    list_display    = ('title', 'value', 'currency', 'current_value_usd', 'continuous', 'country', 'sticky', 'published')
+    readonly_fields = ('current_value_usd', 'inflation_last_year',)
+    search_fields   = ['title', 'value', 'current_value_usd', 'country']
+    form            = forms.StoryForm
 
 
 admin.site.register(models.Story, StoryAdmin)
