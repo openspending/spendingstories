d1949dac48e41092bb23f9dbae17b9830494ba96
Edouard <edou4rd@gmail.com>
2013-08-07 17:37:59 +0200

    admin: actions

--- webapp/core/admin.py
+++ webapp/core/admin.py
@@ -11,15 +11,39 @@
 # Last mod : 07-Aug-2013
 # -----------------------------------------------------------------------------
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 import models
 import forms
 
+# -----------------------------------------------------------------------------
+#
+#    THEME
+#
+# -----------------------------------------------------------------------------
 class ThemeAdmin(admin.ModelAdmin):
     list_display    = ('title', 'description', 'active')
     prepopulated_fields = {"slug": ("title",)}
     list_editable       = ('active',)
 
+# -----------------------------------------------------------------------------
+#
+#    STORY
+#
+# -----------------------------------------------------------------------------
+def make_published(modeladmin, request, queryset):
+    queryset.update(status='published')
+make_published.short_description = _("Mark selected contributions as published")
+
+def make_refused(modeladmin, request, queryset):
+    queryset.update(status='refused')
+make_refused.short_description = _("Mark selected contributions as refused")
+
+def make_pending(modeladmin, request, queryset):
+    queryset.update(status='pending')
+make_pending.short_description = _("Mark selected contributions as pending")
+
 class StoryAdmin(admin.ModelAdmin):
+    actions         = [make_published, make_refused, make_pending]
     list_display    = ('title', 'value', 'currency', 'current_value_usd', 'country', 'sticky', 'created_at', 'status')
     readonly_fields = ('current_value', 'current_value_usd', 'inflation_last_year',)
     search_fields   = ('title', 'value', 'current_value_usd', 'country')
