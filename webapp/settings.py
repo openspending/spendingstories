0d98310affcacf9fea7f65dcba586f42ca550b7d
Edouard <edou4rd@gmail.com>
2013-08-14 17:05:02 +0200

    django.core.context_processors.media added

--- webapp/settings.py
+++ webapp/settings.py
@@ -141,6 +141,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
     'django.contrib.messages.context_processors.messages',
     "django.core.context_processors.debug",
+    "django.core.context_processors.media",
     'django.core.context_processors.static',
 )
 
