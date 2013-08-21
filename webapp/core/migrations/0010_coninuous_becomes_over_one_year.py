b9803edf472c903342d059c388f983be30eccf5c
Edouard <edou4rd@gmail.com>
2013-08-21 12:36:11 +0200

    migrations : continuous becomes over_one_year

---
+++ webapp/core/migrations/0010_coninuous_becomes_over_one_year.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        for story in orm.Story.objects.all():
+            story.type = "over_one_year" if story.type == "continuous" else story.type
+            story.save()
+
+    def backwards(self, orm):
+        for story in orm.Story.objects.all():
+            story.type = "continuous" if story.type == "over_one_year" else story.type
+            story.save()
+
+    models = {
+        u'core.story': {
+            'Meta': {'object_name': 'Story'},
+            'country': ('webapp.core.fields.CountryField', [], {'max_length': '3'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'currency': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['currency.Currency']"}),
+            'current_value': ('django.db.models.fields.FloatField', [], {}),
+            'current_value_usd': ('django.db.models.fields.FloatField', [], {}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'extras': ('django.db.models.fields.TextField', [], {'default': "'{}'"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'inflation_last_year': ('django.db.models.fields.IntegerField', [], {'max_length': '4'}),
+            'source': ('django.db.models.fields.URLField', [], {'max_length': '140'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'pending'", 'max_length': '9'}),
+            'sticky': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'themes': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['core.Theme']", 'symmetrical': 'False'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '240'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'discrete'", 'max_length': '15'}),
+            'value': ('django.db.models.fields.FloatField', [], {}),
+            'year': ('django.db.models.fields.IntegerField', [], {'max_length': '4'})
+        },
+        u'core.theme': {
+            'Meta': {'ordering': "('slug',)", 'object_name': 'Theme'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '500', 'null': 'True', 'blank': 'True'}),
+            'image': ('django.db.models.fields.files.FileField', [], {'max_length': '300', 'null': 'True', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '80'})
+        },
+        u'currency.currency': {
+            'Meta': {'object_name': 'Currency'},
+            'iso_code': ('django.db.models.fields.CharField', [], {'max_length': '3', 'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
+            'rate': ('django.db.models.fields.FloatField', [], {})
+        }
+    }
+
+    complete_apps = ['core']
+    symmetrical = True
