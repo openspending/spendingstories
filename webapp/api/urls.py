0d5b91b2878f08bb268b6c36ab89d845ae27e419
Bellon Pierre <bellon.pierre@gmail.com>
2013-09-19 18:13:49 +0200

    Added storyTypes urls into api/urls.py

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -15,16 +15,18 @@ from rest_framework import routers
 import views
 
 router = routers.DefaultRouter()
-router.register(r'countries'           , views.CountryViewSet, base_name="countries")
-router.register(r'currencies'          , views.CurrencyViewSet)
-router.register(r'meta'                , views.MetaViewSet     , base_name="meta")
-router.register(r'stories'             , views.StoryViewSet)
-router.register(r'stories-nested'      , views.StoryNestedViewSet,  base_name="stories-nested")
-router.register(r'themes'              , views.ThemeViewSet)
-router.register(r'filters/themes'      , views.UsedThemeViewSet,    base_name="used themes")
-router.register(r'filters/countries'   , views.UsedCountryViewSet,  base_name="used countries")
-router.register(r'filters/currencies'  , views.UsedCurrencyViewSet,  base_name="used currencies")
+router.register(r'countries'           , views.CountryViewSet     , base_name="countries"      )
+router.register(r'currencies'          , views.CurrencyViewSet    , base_name="currencies"     )
+router.register(r'meta'                , views.MetaViewSet        , base_name="meta"           )
+router.register(r'stories'             , views.StoryViewSet       , base_name="stories"        )
+router.register(r'stories-nested'      , views.StoryNestedViewSet , base_name="stories-nested" )
+router.register(r'themes'              , views.ThemeViewSet       , base_name="themes"         )
+router.register(r'storyTypes'          , views.StoryTypesViewSet  , base_name="story types"    )
 
+router.register(r'filters/countries'   , views.UsedCountryViewSet    , base_name="used countries"  )
+router.register(r'filters/currencies'  , views.UsedCurrencyViewSet   , base_name="used currencies" )
+router.register(r'filters/storyTypes'  , views.UsedStoryTypesViewSet , base_name="used story types")
+router.register(r'filters/themes'      , views.UsedThemeViewSet      , base_name="used themes"     )
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
