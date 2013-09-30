ddfe4bf340d38c705f09d5dca6235c274cacd6ca
Edouard <edou4rd@gmail.com>
2013-09-30 15:58:21 +0200

    comments

--- webapp/settings.py
+++ webapp/settings.py
@@ -33,12 +33,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dev.db',                      # Or path to database file if using sqlite3.
+        'NAME'    : 'dev.db',                   # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
+        'USER'    : '',
         'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'HOST'    : '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT'    : '', # Set to empty string for default.
     }
 }
 
