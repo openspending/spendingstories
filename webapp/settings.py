6f7b36de7c76a45b43a65b036133c67b5f13869e
Edouard <edou4rd@gmail.com>
2013-10-02 15:59:30 +0200

    licence text

--- webapp/settings.py
+++ webapp/settings.py
@@ -10,6 +10,21 @@
 # Creation : 05-Aug-2013
 # Last mod : 02-Oct-2013
 # -----------------------------------------------------------------------------
+# This file is part of Spending Stories.
+# 
+#     Spending Stories is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+# 
+#     Spending Stories is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with Spending Stories.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 
 ROOT_PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir))
