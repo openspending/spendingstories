f3084cc7678b67b5a0de0420bfc7509fb82aff91
Edouard <edou4rd@gmail.com>
2013-10-07 17:21:03 +0200

    new filters api

--- webapp/api/serializers.py
+++ webapp/api/serializers.py
@@ -25,30 +25,9 @@
 #     You should have received a copy of the GNU General Public License
 #     along with Spending Stories.  If not, see <http://www.gnu.org/licenses/>.
 
-from rest_framework import serializers, filters 
+from rest_framework import serializers 
 from webapp.currency.models import Currency
 from webapp.core.models import Story, Theme
-
-class UsedModelSerializer(serializers.ModelSerializer):
-    used = serializers.SerializerMethodField('is_used')
-    def is_used(self, obj):
-        meta = self.__class__.ModelMeta()
-        kwargs = { meta.queryset_filter: getattr(obj, meta.attribute) }
-        return meta.queryset.filter(**kwargs).count() > 0
-
-class UsedModelFilter(filters.BaseFilterBackend):
-    def filter_queryset(self, request, queryset, view):
-        serializer_class = view.serializer_class()
-        results = []
-        is_used = request.QUERY_PARAMS.get('isUsed', None)
-        if not isinstance(serializer_class, UsedModelSerializer) or is_used is None:
-            results = queryset
-        else:
-            b_is_used = is_used != 'False' and is_used != 'false'
-            for elem in queryset:
-                if view.serializer_class().is_used(elem) == b_is_used:
-                    results.append(elem)
-        return results
 
 class ThemeSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField('get_image')
@@ -59,21 +38,9 @@ class ThemeSerializer(serializers.ModelSerializer):
         """ return the absolute image url """
         return obj.image.url
 
-class UsedThemeSerializer(ThemeSerializer, UsedModelSerializer):
-    class ModelMeta: 
-        attribute = 'slug'
-        queryset_filter = 'themes__slug'
-        queryset = Story.objects.public()
-
 class CurrencySerializer(serializers.ModelSerializer):
     class Meta:
         model = Currency
-
-class UsedCurrencySerializer(CurrencySerializer, UsedModelSerializer):
-    class ModelMeta:
-        queryset = Story.objects.public() 
-        attribute = 'iso_code'
-        queryset_filter = 'currency'
 
 class StorySerializer(serializers.ModelSerializer):
     class Meta:
--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -36,14 +36,11 @@ router.register(r'meta'                , views.MetaViewSet        , base_name="m
 router.register(r'stories'             , views.StoryViewSet       , base_name="stories"        )
 router.register(r'stories-nested'      , views.StoryNestedViewSet , base_name="stories-nested" )
 router.register(r'themes'              , views.ThemeViewSet       , base_name="themes"         )
+router.register(r'filters'              , views.FiltersViewSet    , base_name="filters"        )
 
-router.register(r'filters/countries'   , views.UsedCountryViewSet    , base_name="used countries"  )
-router.register(r'filters/currencies'  , views.UsedCurrencyViewSet   , base_name="used currencies" )
-router.register(r'filters/themes'      , views.UsedThemeViewSet      , base_name="used themes"     )
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
-    # url(r'^meta/$', views.MetaView.as_view(), name='meta'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
     url(r'^', include(router.urls))
