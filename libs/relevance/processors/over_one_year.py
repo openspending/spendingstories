62660f0441fe1e35ab46a00e21ad657195e90f7c
Edouard <edou4rd@gmail.com>
2013-10-22 17:58:46 +0200

    Over one year : per day now succeed tests

--- libs/relevance/processors/over_one_year.py
+++ libs/relevance/processors/over_one_year.py
@@ -42,8 +42,9 @@ class SubProcessor(Processor):
             one_day   = compared_to / 365.25
             one_week  = compared_to / 52
             one_month = compared_to / 12
-            if amount < one_week and amount >= (one_day - one_day * 0.05):
-                relevance = Relevance(6, Relevance.RELEVANCE_TYPE_DAY, int(amount / one_day))
+            if amount < one_week:
+                if  amount > one_day and amount % one_day <= one_day * .1:
+                    relevance = Relevance(6, Relevance.RELEVANCE_TYPE_DAY, int(amount / one_day))
             elif amount < one_month:
                 # compute into weeks
                 if amount >= one_week and amount % one_week <= one_day * 0.25:
