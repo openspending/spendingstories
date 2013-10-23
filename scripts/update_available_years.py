d1d596f65de1cc9dbcadfd9e38d30267736bc685
Edouard <edou4rd@gmail.com>
2013-10-23 12:09:56 +0200

    scripts/update_available_years.py updated

--- scripts/update_available_years.py
+++ scripts/update_available_years.py
@@ -8,7 +8,7 @@
 # License : GNU General Public License
 # -----------------------------------------------------------------------------
 # Creation : 06-Aug-2013
-# Last mod : 07-Aug-2013
+# Last mod : 23-Oct-2013
 # -----------------------------------------------------------------------------
 # This file is part of Spending Stories.
 # 
@@ -27,27 +27,33 @@
 
 """
 
-Create a json file which contains the list of available years for each country
+Create or update a json file which contains the list of available years for each country
 in the `data/cpi/cpi.csv` file.
 
 This json file will be used in order to check the user entries for dates and
 to show dynamically the available dates if the country is known.
 
 """
+
 import csv
 import json
-results = {}
 
-with open("data/cpi/cpi.csv") as cpi_file:
-    spamreader = csv.reader(cpi_file, delimiter=',', quotechar='"')
-    spamreader.next()
-    for row in spamreader:
-        country, code, year, cpi = row
-    	if not code in results:
-    		results[code] = []
-    	results[code].append(int(year))
+if __name__ == "__main__":
 
-with open("data/years_available_per_country.json", "w") as output:
-	output.write(json.dumps(results))
+    results = {}
+
+    with open("data/cpi/cpi.csv") as cpi_file:
+        spamreader = csv.reader(cpi_file, delimiter=',', quotechar='"')
+        spamreader.next()
+        for row in spamreader:
+            country, code, year, cpi = row
+            if not code in results:
+                results[code] = []
+            results[code].append(int(year))
+
+    with open("data/years_available_per_country.json", "w") as output:
+        output.write(json.dumps(results))
+
+    exit()
 
 # EOF
