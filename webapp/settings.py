27a4f8592a93581008099dec701544dc44bc4785
Edouard <edou4rd@gmail.com>
2013-10-23 16:30:23 +0200

    absolute path for database

--- webapp/settings.py
+++ webapp/settings.py
@@ -46,7 +46,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME'    : 'dev.db',                   # Or path to database file if using sqlite3.
+        'NAME'    : ROOT_PATH + '/dev.db', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER'    : '',
         'PASSWORD': '',
