9ae4ad3d0c05c3b022c8ea185668763e36133b7f
pbellon <bellon.pierre@gmail.com>
2013-12-11 19:18:33 +0100

    Fixed i18n custom context processor

--- webapp/context_processors.py
+++ webapp/context_processors.py
@@ -5,8 +5,11 @@ def i18n(request):
     def get_language_code():
         base_code = translation.get_language()
         sub_codes = base_code.split('-')
-        sub_codes[1] = sub_codes[1].upper()
-        return "_".join(sub_codes)
+        if len(sub_codes) > 1:
+            sub_codes[1] = sub_codes[1].upper()
+            return "_".join(sub_codes)
+        else:
+            return base_code
 
     context_extras = {}
     context_extras['LANG'] = get_language_code()
